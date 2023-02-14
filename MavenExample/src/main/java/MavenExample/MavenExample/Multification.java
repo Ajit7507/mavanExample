286dec33826d0fc9e4a78b664d765a5be2abfbd7
developers <developers@linux>
2023-02-14 15:14:44 +0530

    mul

---
+++ MavenExample/src/main/java/MavenExample/MavenExample/Multification.java
@@ -0,0 +1,10 @@
+package MavenExample.MavenExample;
+
+public class Multification {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(100);
+	}
+
+}
